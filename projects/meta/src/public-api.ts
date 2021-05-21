fd3cede5fddec5d6fc624dc72cf9c79963b003b9
vdumbrava <vdumbrava@starnet.md>
2021-05-21 18:09:16 +0300

    fix version

--- projects/meta/src/public-api.ts
+++ projects/meta/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/meta.service';
 export * from './lib/meta.module';
 export * from './lib/meta.loader';
+export * from './lib/models/page-title-positioning';
+export * from './lib/models/meta-settings';
+
